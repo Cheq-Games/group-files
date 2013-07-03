d080f1a7db4a67b6ef1e00814da445d22a76f86e
Glenford Williams <glenford.williams@samsharpe.edu.jm>
2013-07-03 16:17:37 -0500

    modified:   README.md 	new file:   small game/README.md 	new file:   small game/include/game.hpp 	new file:   small game/include/object.hpp 	new file:   small game/main.cpp 	new file:   small game/src/game.cpp 	new file:   small game/src/object.cpp

---
+++ small game/src/object.cpp
@@ -0,0 +1,81 @@
+#include "../include/object.hpp"
+
+//TODO provide a way to delete object 
+//TODO add comments
+
+
+
+Object::Object(string name, sf::RenderWindow &win)
+{
+    this->objName = name;
+    loaded = false;
+}
+
+Object::Object(string name,int posx, int posy, sf::RenderWindow &win)
+{
+    loaded = false;
+    loadObject(name);
+    _sprite.setPosition(posx, posy);
+}
+
+void Object::draw(sf::RenderWindow &win)
+{
+   win.draw(_sprite);
+   
+}
+
+sf::Vector2f Object::getPosition()
+{
+	return _sprite.getPosition();
+}
+
+
+void Object::loadObject(string name)
+{
+
+    if(_texture.loadFromFile(name) == false)
+    {
+        loaded = false;  
+        cout<<"error loading file: "<<name<<endl;        
+    }
+    else
+    {
+        loaded = true; 
+        this->objName = name;
+        _sprite.setTexture(_texture);
+    }
+
+}
+
+void Object::loadObject()
+{
+
+    //TODO check if loadObject(string name) was used in a previous operation to avo
+    if(_texture.loadFromFile(this->objName) == false)
+    {
+        cout<<"error loading file: "<<this->objName<<endl;        
+    }
+    else
+    {
+           loaded = true;
+           _sprite.setTexture(_texture);
+    }
+}
+
+void Object::move(int xpos, int ypos)
+{
+    if(loaded)
+    {
+		_sprite.move(xpos, ypos);    
+    }
+}
+
+void Object::setPosition(int posx, int posy)
+{
+    if(loaded)
+    {
+		_sprite.setPosition( posx, posy);
+    }
+}
+
+//object.cpp
